756cae58b98c3df6cf592abca4e1e1cd21b7ea6a
MartinTJahn <MartinTJahn@users.noreply.github.com>
2024-03-19 15:26:15 +0000

    Update text2mp3.sh

--- text2mp3.sh
+++ text2mp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+
 # Modified from https://gist.github.com/kentbye/3843248 with j
-# Converts a paper to .mp3 with 150 words per minute 
 
 # Dependencies on Mac
 #brew install lynx
@@ -9,8 +9,7 @@
 # Prep steps:
 	# Copy weblink to paper to clipboard
 	# run script on bash "text2mp3_v1.sh -t Title -i WebLink"
-    # see examples below
-
+    	# see usage examples below
 
 #usage="text2mp3 [-h] [-j -l -t] -- program to transform papers into mp3
 #where:
@@ -18,7 +17,6 @@
 #    -j  set journal format [NatCom,NatRevMicro,Nature,MucosalImmo] 
 #    -l  set link to paper 
 #    -t  mp3 title"
-
 
 while getopts t:l:j: option
 do
@@ -30,9 +28,9 @@ j) JOURNAL=${OPTARG};;
 esac
 done
 
-
 echo $TITLE
 echo $LINK
+
 
 # Format papers
 
@@ -74,7 +72,6 @@ else
    echo "Unknown parameter"
 fi
 
-
 echo "converting $LINK to .aiff";
 #`say -f tmp.txt -r 150 -o $FILE.aiff --progress`; # system default
 `say -v "Serena" -f $TITLE.txt -r 150 -o tmp.aiff --progress`; # Kate
@@ -84,32 +81,3 @@ echo "conververting to .mp3";
 
 rm tmp.aiff
 
-
-# Example use
-
-'''
-
-## NatCom
-#bash text2mp3_v6_directfromweb.sh  "testi" "https://www.nature.com/articles/s41467-021-25073-w"
-
-## NatRevMicro
-#bash text2mp3_v6_directfromweb.sh  -j "NatRevMicro" -t "Galan_Samonella_and_Inflammation" -l "https://www.nature.com/articles/s41579-021-00561-4"
-
-# Nature
-#bash text2mp3_v6_directfromweb.sh  -j "Nature" -t "Baeumler_et_al_Interactions_between_microbiota_and_pathogen_in_gut" -l "https://www.nature.com/articles/nature18849"
-
-## Mucosal Immunity
-#bash text2mp3_v6_directfromweb.sh  "Fattinger_Salmonella_Induced_tissue_destruction" "https://www.nature.com/articles/s41385-021-00381-y#Sec10"
-
-
-#bash text2mp3_v6_directfromweb.sh  -j "Nature" -t "Baeumler_et_al_Interactions_between_microbiota_and_pathogen_in_gut" -l "https://www.nature.com/articles/nature18849"
-#bash text2mp3_v6_directfromweb.sh -l "https://www.nature.com/articles/npjbiofilms20155" -j "MucosalImmo" -t "deVos_2014" 
-#bash text2mp3_v6_directfromweb.sh -t "Worrich_et_al_2019_Associational_effects_in_the_microbial_neighborhood" -l https://www.nature.com/articles/s41396-019-0444-6 -j "ISMEJ"
-#bash text2mp3_v6_directfromweb.sh -t "WEISS_2021_ISMEJ" -l "https://www.nature.com/articles/s41396-021-01153-z" -j "ISMEJ"
-#bash text2mp3_v6_directfromweb.sh  -j "NatRevMicro" -t "Sorbara_2022_Microbiome_based_therapeutics_NatRevMicro" -l "https://www.nature.com/articles/s41579-021-00667-9"
-#bash text2mp3_v6_directfromweb.sh  -j "NatCom" -t "Monaco_2022_spatioTemp_and_cheatinf_NatCom" -l "https://www.nature.com/articles/s41467-022-28321-9"
-#bash text2mp3_v6_directfromweb.sh  -j "NatCom" -t "Kauffman_2022_StructureInPhage-bacteria_interct_NatCom" -l "https://www.nature.com/articles/s41467-021-27583-z"
-
-#bash text2mp3_v6_directfromweb.sh  -j "NatRevMicro" -t "Tropini_Gut_Geo_NatRevMicro" -l "https://www.nature.com/articles/s41579-023-00969-0"
-
-'''
